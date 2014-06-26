22f8b82cc2030e825a434258ea49780d1dd526e1
mfryf <mfr_2008@qq.com>
2014-06-26 14:14:58 +0800

    Update change_version.sh
    增加更新后的版本号输出

--- blog/program/change_version.sh
+++ blog/program/change_version.sh
@@ -8,3 +8,4 @@ cat mengfanrong/csdnblog_publish.py|sed "s;version=\"$version\";version=\"$new_v
 cp tmp.py mengfanrong/csdnblog_publish.py
 ./bat_generate.sh
 rm tmp.py
+echo "version:"`cat version`
